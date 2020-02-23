f082fbb0f5791d38c84e21d9d6dcf9a9db73ebcd
tabiouch03 <54985916+tabiouch03@users.noreply.github.com>
2020-02-23 23:53:13 +0100

    update TP

--- TP-JS-YANNICK/TPnombrePhotocopie/script.js
+++ TP-JS-YANNICK/TPnombrePhotocopie/script.js
@@ -1,11 +1,16 @@
-let nbcopie = 30;
 
-if (nbcopie <= 10) {
-    prix = nbcopie * 0.1;
-} else if (nbcopie > 11) {
-    prix = (10*0.1) + ((nbcopie - 11) * 0.09);
-} else if (nbcopie > 20) {
-    prix = (10 * 0.1) + (10*0.09) + ((nbcopie - 20) * 0.08);
+function photocopie() {
+
+    let nbcopie = document.getElementById("photo").value;
+
+    if (nbcopie <= 10) {
+        prix = nbcopie * 0.1;
+    } else if (nbcopie > 10) {
+        prix = (10 * 0.1) + ((nbcopie - 10) * 0.09);
+    } else if (nbcopie > 20) {
+        prix = (10 * 0.1) + (20 * 0.09) + ((nbcopie - 30) * 0.08);
+    }
+
+    alert(prix);
+
 }
-
-console.log(prix);
